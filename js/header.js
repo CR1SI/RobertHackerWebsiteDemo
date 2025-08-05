12c6763c9d6c59965d6ce9c4940cca6014173b09
CR1SI <cristianpmason@icloud.com>
2025-08-05 08:51:57 -0400

    headers and pages

--- js/header.js
+++ js/header.js
@@ -1,10 +1,83 @@
-const myHeader = document.getElementById('header');
-myHeader.innerHTML = `
-    <div class="flex items-center justify-center sm:col-span-6">
-        <h6 class="text-6xl font-extrabold text-gray-700">WEBSITE NAME</h6>
-    </div>
-    <div class="grid items-center justify-items-center m-4 gap-4 sm:grid-cols-3 sm:col-span-6  ">
-        <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">BOOKS</button>
-        <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">ABOUT</button>
-        <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">CONTACT</button>
-    </div>`;
+const myHeaderH = document.getElementById('headerH');
+if(myHeaderH){
+    myHeaderH.innerHTML = `
+        <div class="flex items-center justify-center sm:col-span-6">
+            <a href="index.html">
+                <h6 class="text-6xl font-extrabold text-gray-700">LANDING PAGE</h6>
+            </a>
+        </div>
+        <div class="grid items-center justify-items-center m-4 gap-4 sm:grid-cols-3 sm:col-span-6  ">
+            <a href="books.html">
+                <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">BOOKS</button>
+            </a>
+            <a href="about.html">
+                <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">ABOUT</button>
+            </a>
+            <a href="contact.html">
+                <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">CONTACT</button>
+            </a>
+        </div>`;
+}
+
+const myHeaderB = document.getElementById('headerB');
+if(myHeaderB){
+    myHeaderB.innerHTML = `
+        <div class="flex items-center justify-center sm:col-span-6">
+            <a href="index.html">
+                <h6 class="text-6xl font-extrabold text-gray-700">BOOK PAGE</h6>
+            </a>
+        </div>
+        <div class="grid items-center justify-items-center m-4 gap-4 sm:grid-cols-3 sm:col-span-6  ">
+            <a href="books.html">
+                <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">BOOKS</button>
+            </a>
+            <a href="about.html">
+                <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">ABOUT</button>
+            </a>
+            <a href="contact.html">
+                <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">CONTACT</button>
+            </a>
+        </div>`;
+}
+
+const myHeaderC = document.getElementById('headerC');
+if(myHeaderC){
+    myHeaderC.innerHTML = `
+        <div class="flex items-center justify-center sm:col-span-6">
+            <a href="index.html">
+                <h6 class="text-6xl font-extrabold text-gray-700">CONTACT PAGE</h6>
+            </a>
+        </div>
+        <div class="grid items-center justify-items-center m-4 gap-4 sm:grid-cols-3 sm:col-span-6  ">
+            <a href="books.html">
+                <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">BOOKS</button>
+            </a>
+            <a href="about.html">
+                <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">ABOUT</button>
+            </a>
+            <a href="contact.html">
+                <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">CONTACT</button>
+            </a>
+        </div>`;
+}
+
+const myHeaderA = document.getElementById('headerA');
+if (myHeaderA){
+    myHeaderA.innerHTML = `
+        <div class="flex items-center justify-center sm:col-span-6">
+            <a href="index.html">
+                <h6 class="text-6xl font-extrabold text-gray-700">ABOUT PAGE</h6>
+            </a>
+        </div>
+        <div class="grid items-center justify-items-center m-4 gap-4 sm:grid-cols-3 sm:col-span-6  ">
+            <a href="books.html">
+                <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">BOOKS</button>
+            </a>
+            <a href="about.html">
+                <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">ABOUT</button>
+            </a>
+            <a href="contact.html">
+                <button class="h-[40px] w-[150px] rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 hover:shadow-lg transition-transform duration-200 ease-in-out active:scale-95 bg-gray-700 text-white">CONTACT</button>
+            </a>
+        </div>`;
+}
